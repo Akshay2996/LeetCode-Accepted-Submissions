53ac3ca596fa467b2577e7efe4cad8e6d2474d23
Akshay Sharma <akshaysharmaay@gmail.com>
2026-03-30 11:12:25 +0530

    Add: sqrtx - submission-13

---
+++ Data Structures & Algorithms/sqrtx/submission-13.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    // Binary Search -> Time - O(logn) and Space - O(1)
+    mySqrt(x) {
+        let l = 0;
+        let r = x;
+        let res = 0;
+
+        while(l<=r){
+            const mid = l + Math.floor((r-l)/2);
+
+            if(mid*mid > x){
+                r = mid - 1;
+            } else if(mid*mid < x){
+                res = mid;
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return res;
+    }
+}
