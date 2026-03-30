1404f99594a99a10f8059eaf4cbc83a2430c37c8
Akshay Sharma <akshaysharmaay@gmail.com>
2026-03-30 21:23:11 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    // Iteration -> Time - O(n) and Space - O(1)
+    plusOne(digits) {
+        let carry = 1;
+        let i = 0;
+        digits.reverse();
+
+        while(carry){
+            if(i < digits.length){
+                if(digits[i] === 9){
+                    digits[i] = 0;
+                } else {
+                    digits[i] += 1;
+                    carry = 0;
+                }
+            } else {
+                digits.push(carry);
+                carry = 0;
+            }
+            i++;
+        }
+
+        digits.reverse();
+        return digits;
+    }
+}
