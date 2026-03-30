892e90243ce01fb72e2842a20038ba81d98f1bf9
Akshay Sharma <akshaysharmaay@gmail.com>
2026-03-30 12:39:09 +0530

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    // Hashset -> Time - O(logn) and Space - O(logn)
+    isHappy(n) {
+        const seen = new Set();
+
+        while(!seen.has(n)){
+            seen.add(n);
+
+            n = this.sumOfSquares(n);
+            
+            if(n === 1) return true;
+        }
+
+        return false;
+    }
+
+    // Compute 100 -> 1^2 + 0^2 + 0^2 = 1
+    // Compute 101 -> 1^2 + 0^2 + 1^2 = 2 -> 2^2 = 4 and so on...
+    sumOfSquares(n){
+        let output = 0;
+
+        while(n > 0){
+            let rem = n % 10;
+            rem = rem * rem;
+            output += rem;
+            n = Math.floor(n / 10);
+        }
+
+        return output;
+    }
+}
