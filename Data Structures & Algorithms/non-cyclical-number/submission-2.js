0e137058600d137e32ba5fbb249b5f0f5f246065
Akshay Sharma <akshaysharmaay@gmail.com>
2026-03-30 12:23:28 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    // Fast and Slow Pointers -> Time - O(logn) and Space - O(1)
+    // Think of this problem like linked list where 
+    // this.sumOfSquares(n) getting called is moving through the list
+    isHappy(n) {
+        let slow = n;
+        let fast = this.sumOfSquares(n);
+        
+        while(fast !== slow){
+            slow = this.sumOfSquares(slow);
+            fast = this.sumOfSquares(fast);
+            fast = this.sumOfSquares(fast);
+        }
+
+        return fast === 1;
+    }
+
+    sumOfSquares(n){
+        let output = 0;
+        
+        while(n > 0){
+            let rem = n % 10;
+            rem = rem * rem;
+            output += rem;
+            n = Math.floor(n / 10);
+        }
+
+        return output;
+    }
+}
