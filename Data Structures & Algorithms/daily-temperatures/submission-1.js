667ea404ca4fa5ce1f6950281cb47f63dcfbb365
Akshay Sharma <akshaysharmaay@gmail.com>
2026-04-04 13:48:16 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    // Stack -> Time - O(n) and Space - O(n)
+    dailyTemperatures(temperatures) {
+        const res = new Array(temperatures.length).fill(0);
+        const stack = []; // Store in pairs: [temp, index]
+
+        for(let i = 0; i < temperatures.length; i++){
+            const t = temperatures[i];
+            
+            // Loop only works when temp > the top of the stack element
+            while(stack.length > 0 && t > stack[stack.length - 1][0]){
+                const [stackTemp, stackIndex] = stack.pop();
+                // Difference between the current index and the stack index of the popped item
+                // Store in the res array
+                res[stackIndex] = i - stackIndex;
+            }
+
+            stack.push([t, i]);
+        }
+
+        return res;
+    }
+}
