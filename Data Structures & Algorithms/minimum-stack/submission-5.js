f610db5b39e7b873c788e4ca8c9cf253add35817
Akshay Sharma <akshaysharmaay@gmail.com>
2026-04-04 12:39:00 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.js
@@ -0,0 +1,56 @@
+/**
+ * Use of 2 stacks -> MinStack and Stack
+ * Time Complexity
+ *   push() - O(1)
+ *   pop() - O(1)
+ *   top() - O(1)
+ *   getMin() - O(1)
+ * Space Complexity - O(n) 
+ */
+
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        
+        if(this.minStack.length === 0){
+            this.minStack.push(val);
+        } else {
+            const minVal = Math.min(this.minStack[this.minStack.length - 1], val);
+
+            this.minStack.push(minVal);
+        }
+
+
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+       return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
