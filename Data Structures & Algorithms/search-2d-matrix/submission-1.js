36beb5c9df90c0c7163e98c5261a30109b39e3ad
Akshay Sharma <akshaysharmaay@gmail.com>
2026-04-07 11:24:51 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    // Bruteforce -> Time - O(m*n) and Space - O(1)
+    searchMatrix(matrix, target) {
+        for(let r = 0; r < matrix.length; r++){
+            for(let c = 0; c < matrix[r].length; c++){
+                if(matrix[r][c] === target){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
