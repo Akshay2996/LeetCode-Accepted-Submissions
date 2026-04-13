673462dfa84ba70b795d7894ad788b672f52a6f1
Akshay Sharma <akshaysharmaay@gmail.com>
2026-04-13 13:52:16 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    // Sliding Window -> Time - O(n)
+    maxProfit(prices) {
+        let l = 0;
+        let r = 1;
+        let maxP = 0;
+
+        while(r < prices.length){
+            // If condition is true then
+            if(prices[l] < prices[r]){
+                maxP = Math.max(maxP, prices[r] - prices[l]);
+            } 
+            // Condition is false then move the left pointer
+            else {
+                l = r;
+            }
+            r++;
+        }
+
+        return maxP;
+    }
+}
