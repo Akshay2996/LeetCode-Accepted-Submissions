c34fabdc66d4b9af41e5e82dbf66846718449970
Akshay Sharma <akshaysharmaay@gmail.com>
2026-04-23 21:03:20 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.js
@@ -0,0 +1,59 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    // Sliding Window -> Time - O(n + m) and Space O(m)
+    // Where n is the length of the string s and
+    // m is the total number of unique characters in the strings t and s.
+    minWindow(s, t) {
+        // Edge case
+        if(t === "") return "";
+
+        const countT = {};
+        const window = {};
+
+        // Populate the object -> character (value): frequency (count)
+        for(let c of t){
+            countT[c] = (countT[c] || 0) + 1;
+        }
+
+        let l = 0, r = 0;
+        let have = 0;
+        let need = Object.keys(countT).length;
+        let res = [-1, -1];
+        let resLen = Infinity;
+
+        while(r < s.length){
+            let c = s[r];
+            window[c] = (window[c] || 0) + 1;
+
+            if(countT[c] && countT[c] === window[c]){
+                have++;
+            }
+
+            while(have === need){
+                //  Checking the minimum length then updating the result
+                 if(r - l + 1 < resLen){
+                    resLen = r - l + 1;
+                    res = [l, r];
+                 }
+
+                 window[s[l]]--;
+
+                 if(countT[s[l]] && countT[s[l]] > window[s[l]]){
+                    have--;
+                 }
+
+                 l++;
+            }
+
+            r++;
+        }
+        
+        [l, r] = res;
+
+        return resLen === Infinity ? "" : s.slice(l, r + 1);
+    }
+}
