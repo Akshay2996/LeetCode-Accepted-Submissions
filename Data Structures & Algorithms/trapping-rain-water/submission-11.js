03554ceac71881c83393a0e103835510b25eab6c
Akshay Sharma <akshaysharmaay@gmail.com>
2026-04-24 17:37:24 +0530

    Add: trapping-rain-water - submission-11

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-11.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    // 2 pointers -> Time - O(n) & Space - O(1)
+    // Formula = min(leftMax, rightMax) - height[i];
+    trap(height) {
+        if (!height || height.length === 0) return 0;
+
+        const n = height.length;
+        let l = 0;
+        let r = n - 1;
+        let res = 0;
+        let leftMax = height[0];
+        let rightMax = height[n - 1];
+
+        while (l < r) {
+            if(leftMax < rightMax){
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                res += leftMax - height[l];
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                res += rightMax - height[r];
+            }
+        }
+
+        return res;
+    }
+}
